fa93ecabb39a330b6eee1edc96e36362dc9d8728
yar0slava <yavacollins@gmail.com>
2021-05-23 21:52:43 +0300

    initial commit

---
+++ server/src/main/java/kma/practice/eambulance/EAmbulanceApplication.java
@@ -0,0 +1,13 @@
+package kma.practice.eambulance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAmbulanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAmbulanceApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/kma/practice/eambulance/EAmbulanceApplicationTests.java
@@ -0,0 +1,13 @@
+package kma.practice.eambulance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAmbulanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
