a0c1883bb23a42d23ee9b02cc369e099ccff9dc9
Serhii Vakulenko <sergeyvakulenko22@gmail.com>
2021-06-13 22:22:55 +0300

    before pull

--- server/src/main/java/kma/practice/eambulance/EAmbulanceApplication.java
+++ server/src/main/java/kma/practice/eambulance/EAmbulanceApplication.java
@@ -1,7 +1,9 @@
 package kma.practice.eambulance;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class EAmbulanceApplication {
@@ -9,5 +11,6 @@ public class EAmbulanceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EAmbulanceApplication.class, args);
 	}
+
 
 }
