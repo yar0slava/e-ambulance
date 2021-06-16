28f505d3dddeeb20d73770e74c9ff02e40cec9e6
yar0slava <yavacollins@gmail.com>
2021-06-17 01:23:34 +0300

    routing for dispatcher; tab icon

--- client/src/e-ambulance/src/app/app.module.ts
+++ client/src/e-ambulance/src/app/app.module.ts
@@ -5,22 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { DispatcherNavbarComponent } from './dispatcher-navbar/dispatcher-navbar.component';
 import { DispatcherInfoComponent } from './dispatcher-info/dispatcher-info.component';
 import { CallInfoComponent } from './call-info/call-info.component';
 import { CallsListComponent } from './calls-list/calls-list.component';
 import { NewCallComponent } from './new-call/new-call.component';
+import { CrewInfoComponent } from './crew-info/crew-info.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    DispatcherNavbarComponent,
     DispatcherInfoComponent,
     CallInfoComponent,
     CallsListComponent,
-    NewCallComponent
+    NewCallComponent,
+    CrewInfoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
