4ed4c80795b92b0d06e10df24a581d40aeab21e4
yar0slava <yavacollins@gmail.com>
2021-06-21 18:59:34 +0300

    login on fronted

---
+++ client/src/e-ambulance/src/app/login.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Credentials} from "./models/credentials";
+import {catchError, tap} from "rxjs/operators";
+import {LoginResponse} from "./models/login-response";
+import {LocalStorageService} from "./local-storage.service";
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private loginUrl = 'http://localhost:8080/login';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor( private http: HttpClient,
+               private localStorageService: LocalStorageService,
+               private router: Router) {
+  }
+
+  login(credentials: Credentials): Observable<any> {
+    return this.http.post<LoginResponse>(this.loginUrl, credentials, this.httpOptions).pipe(
+      tap((response: LoginResponse) => {
+        this.localStorageService.set('JWTToken',response.token);
+        this.localStorageService.set('authority',response.authority);
+        this.router.navigate(['home'])
+      }));
+  }
+}
